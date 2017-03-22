c3df117b55c4093c8cc79af184531e1bcbabdee1
Krishnanand Sivaraj <krishnanand.sivaraj@softcrylic.co.in>
2017-03-22 21:26:42 +0530

    adding the files for commit

---
+++ ProfilerApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using ProfilerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using EFlogger.EntityFramework6;
+
+namespace ProfilerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            EFloggerFor6.Initialize();
+            EFloggerFor6.StartSendToClient();
+
+            using (var test = new testEntities()) {
+                var updatedUser = test.students.SingleOrDefault(x => x.StudentName == "John");
+            }
+
+            using (var test = new testEntities())
+            {
+                var updatedUser = test.students.SingleOrDefault(x => x.StudentName == "Jane");
+            }
+
+            using (var test = new testEntities())
+            {
+                var updatedUser = test.students.SingleOrDefault(x => x.Grade == "3");
+            }
+
+            using (var test = new testEntities())
+            {
+                var updatedUser = test.students.SingleOrDefault(x => x.Grade == "4");
+            }
+
+            using (var test = new testEntities())
+            {
+                var updatedUser = test.students.SingleOrDefault(x => x.StudentDescription == "John Doe");
+            }
+
+            using (var test = new testEntities())
+            {
+                var updatedUser = test.students.SingleOrDefault(x => x.StudentDescription == "Jane Austin");
+            }
+            EFloggerFor6.StopSendToClient();
+            return View();
+            
+        }
+    }
+}
---
+++ ProfilerApp/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProfilerApp.Models
+{
+    [Table("students")]
+    public class Student
+    {
+        [Key]
+        [Column("EnrolId")]
+        public int EnrolId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string Grade { get; set; }
+
+        public string StudentDescription { get; set; }
+
+        public DateTime JoiningDate { get; set; }
+    }
+}
